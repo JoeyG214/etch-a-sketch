2006913263b681812b130d95dd9580e6e64819a6
Joey <erjo.guce214@gmail.com>
2022-08-22 09:00:40 -0400

    Added button event listeners and activeButton function

--- script.js
+++ script.js
@@ -1,14 +1,51 @@
 const gridContainer = document.querySelector('.grid-container');
 const range = document.querySelector('#range');
 const gridSize = document.querySelector('.grid-size');
-
-
+const colorButton = document.querySelector('.color-button');
+const rainbowButton = document.querySelector('.rainbow-button');
+const eraseButton = document.querySelector('.erase-button');
+const clearButton = document.querySelector('.clear-button');
 
 range.addEventListener('change', () => {
     clearGrid();
     changeGridDimensionsText(range.value);
-    createGrid(range.value)
+    createGrid(range.value);
 });
+
+colorButton.addEventListener('click', () => {
+    activeButton(colorButton);
+});
+
+rainbowButton.addEventListener('click', () => {
+    activeButton(rainbowButton);
+});
+
+eraseButton.addEventListener('click', () => {
+    activeButton(eraseButton);
+});
+
+clearButton.addEventListener('click', () => {
+
+});
+
+function activeButton(selectedButton) {
+    if(selectedButton === colorButton) {
+        colorButton.classList.add('active');
+        rainbowButton.classList.remove('active');
+        eraseButton.classList.remove('active');
+    }
+    else if (selectedButton === rainbowButton) {
+        rainbowButton.classList.add('active');
+        colorButton.classList.remove('active');
+        eraseButton.classList.remove('active');
+    }
+    else if (selectedButton === eraseButton) {
+        eraseButton.classList.add('active');
+        colorButton.classList.remove('active');
+        rainbowButton.classList.remove('active');
+
+    }
+}
 
 function changeGridDimensionsText(dim) {
     gridSize.textContent = `${dim} x ${dim}`;
