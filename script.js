d81b4810c8c5175285dc8e29aa7a3188576ad586
Joey <erjo.guce214@gmail.com>
2022-08-22 08:13:27 -0400

    Added createGrid function

--- script.js
+++ script.js
@@ -4,11 +4,20 @@ const gridSize = document.querySelector('.grid-size');
 
 
 range.addEventListener('change', () => {
-    changeGridDimensionsText(range.value)
+    changeGridDimensionsText(range.value);
+    createGrid(range.value)
     // console.log(range.value);
-    //Function to change grid dimensions
-})
+});
 
 function changeGridDimensionsText(dim) {
     gridSize.textContent = `${dim} x ${dim}`;
 }
+
+function createGrid(dim) {
+    for(let i = 0; i < dim * dim; i++) {
+        const gridElement = document.createElement('div');
+        gridElement.classList.add('grid-item');
+        gridContainer.appendChild(gridElement);
+    }
+}
+
